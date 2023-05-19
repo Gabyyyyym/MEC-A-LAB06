769e6e85fad49f09bd9a8fde3270f9429c92de41
Gabyyyyym <123745015+Gabyyyyym@users.noreply.github.com>
2023-05-19 10:29:52 -0500

    Add files via upload
    Avance 2

---
+++ Lab06/src/jlabel1.java
@@ -0,0 +1,20 @@
+
+import javax.swing.Icon;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Usuario
+ */
+class jlabel1 {
+
+    static void setIcon(Icon uno) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
